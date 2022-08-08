733237e07334aa19b43d32aaaf5834a018b168fb
Elmorshedy17 <morooo.nabil17@gmail.com>
2022-08-08 16:04:39 +0200

    prepared for ios upload

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,11 @@
 import UIKit
 import Flutter
+import Firebase
+import FirebaseCore
+import FirebaseMessaging
+import UserNotificationsUI
+import GoogleMaps
+
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,7 +13,48 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+  if #available(iOS 10.0, *) {
+    UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+  }
+    self.setUpRemoteNotification(application: application)
+    GMSServices.provideAPIKey("AIzaSyBNe8bkPqyudZowZhxnL1O7o4GCd7HqHk4")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+      fileprivate func setUpRemoteNotification(application: UIApplication) {
+          FirebaseApp.configure()
+          if #available(iOS 10.0, *) {
+              UNUserNotificationCenter.current().delegate = self
+              let authOptions: UNAuthorizationOptions = [.badge, .alert, .sound]
+              UNUserNotificationCenter.current().requestAuthorization(options: authOptions, completionHandler: { _, _ in })
+              Messaging.messaging().delegate = self
+          } else {
+              Messaging.messaging().delegate = self
+              let settings = UIUserNotificationSettings(types: [.badge, .sound, .alert], categories: nil)
+              application.registerUserNotificationSettings(settings)
+          }
+          application.registerForRemoteNotifications()
+      }
+}
+
+extension AppDelegate: MessagingDelegate {
+func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+    print("Firebase registration token: \(fcmToken)")
+}
+    override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {}
+
+func messaging(_ messaging: Messaging, didReceive remoteMessage: MessagingDelegate) {
+    print("Received data message: \(remoteMessage.description)")
+}}
+
+extension AppDelegate
+{
+    // Receive displayed notifications for iOS 10 devices.
+    @available(iOS 10.0, *)
+    override func userNotificationCenter(_ center: UNUserNotificationCenter,
+                                         willPresent notification: UNNotification,
+                                         withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.alert, .badge, .sound])
+    }
 }
